0e2f69d0efc13ab00146726868a406de84ca6c88
Jéssica Quirino <jessica.quirino@prestadores.uhgbrasil.com.br>
2023-09-13 17:16:52 -0300

    Fix: Change Browser to hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { RoutesConfig } from "./routes";
 
 function App() {
  return (
   <>
-   <BrowserRouter>
+   <HashRouter>
     <RoutesConfig />
-   </BrowserRouter>
+   </HashRouter>
   </>
  );
 }
