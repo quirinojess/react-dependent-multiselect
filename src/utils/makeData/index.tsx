6c0cc3c442265a1bf5bcb95fec49dedb451a557f
Jéssica Quirino <jessica.quirino@prestadores.uhgbrasil.com.br>
2023-09-13 16:21:30 -0300

    Feature: Add new data to makeData

--- src/utils/makeData/index.tsx
+++ src/utils/makeData/index.tsx
@@ -1,26 +1,48 @@
 function carsBrands() {
  return [
-  { label: "Volkswagen", value: "volkswagen" },
-  { label: "Fiat", value: "fiat" },
-  { label: "Volvo", value: "volvo" },
+  { label: "Volkswagen", value: "Volkswagen" },
+  { label: "Fiat", value: "Fiat" },
+  { label: "Volvo", value: "Volvo" },
+  { label: "Renault", value: "Renault" },
  ];
 }
 
 function carsModels(brands: string[]) {
  const optionsModels: Array<{ label: string; value: string }> = [];
 
- if (brands.includes("volkswagen")) {
-  optionsModels.push({ label: "Fusca", value: "Fusca" });
- }
-
- if (brands.includes("fiat")) {
-  optionsModels.push({ label: "Onix", value: "Onix" });
- }
-
- if (brands.includes("volvo")) {
+ if (brands.includes("Volkswagen")) {
   optionsModels.push(
-   { label: "c30", value: "c30" },
-   { label: "xc60", value: "xc60" }
+   { label: "Fusca", value: "Fusca" },
+   { label: "Polo", value: "Polo" },
+   { label: "Nivus", value: "Nivus" },
+   { label: "Tcross", value: "TCross" }
+  );
+ }
+
+ if (brands.includes("Fiat")) {
+  optionsModels.push(
+   { label: "Onix", value: "Onix" },
+   { label: "Mobi", value: "Mobi" },
+   { label: "Toro", value: "Toro" },
+   { label: "Pulse", value: "Pulse" }
+  );
+ }
+
+ if (brands.includes("Volvo")) {
+  optionsModels.push(
+   { label: "C40", value: "C40" },
+   { label: "XC40", value: "XC40" },
+   { label: "XC60", value: "XC60" },
+   { label: "S60", value: "S60" }
+  );
+ }
+
+ if (brands.includes("Renault")) {
+  optionsModels.push(
+   { label: "Kwid", value: "Kwid" },
+   { label: "Clio", value: "Clio" },
+   { label: "Sandero", value: "Sandero" },
+   { label: "Duster", value: "Duster" }
   );
  }
 
@@ -31,25 +53,72 @@ function carsColors(models: string[]) {
  const optionsColors: Array<{ label: string; value: string }> = [];
 
  if (models.includes("Fusca")) {
-  optionsColors.push({ label: "blue", value: "Blue" });
+  optionsColors.push(
+   { label: "Blue", value: "Blue" },
+   { label: "Red", value: "Red" }
+  );
+ }
+
+ if (models.includes("Polo")) {
+  optionsColors.push({ label: "Satin", value: "Satin" });
+
+  if (models.includes("Nivus")) {
+   optionsColors.push({ label: "Blue dark", value: "Blue dark" });
+  }
+ }
+
+ if (models.includes("TCross")) {
+  optionsColors.push({ label: "Sunset", value: "Sunset" });
  }
 
  if (models.includes("Onix")) {
   optionsColors.push(
-   { label: "blue", value: "Blue" },
-   { label: "green", value: "Green" }
+   { label: "Blue satin", value: "Blue satin" },
+   { label: "Green", value: "Green" }
   );
  }
 
- if (models.includes("c30")) {
+ if (models.includes("Toro")) {
+  optionsColors.push({ label: "Dark", value: "Dark" });
+ }
+ if (models.includes("Mobi")) {
+  optionsColors.push({ label: "Purple Deep", value: "Purple Deep" });
+ }
+
+ if (models.includes("Pulse")) {
+  optionsColors.push({ label: "Pink", value: "Pink" });
+ }
+
+ if (models.includes("C40")) {
   optionsColors.push({ label: "Yellow", value: "Yellow" });
  }
 
- if (models.includes("xc60")) {
-  optionsColors.push(
-   { label: "grey", value: "Grey" },
-   { label: "purple", value: "Purple" }
-  );
+ if (models.includes("XC40")) {
+  optionsColors.push({ label: "grey", value: "Grey" });
+ }
+
+ if (models.includes("XC60")) {
+  optionsColors.push({ label: "purple", value: "Purple" });
+ }
+
+ if (models.includes("S60")) {
+  optionsColors.push({ label: "Mint", value: "Mint" });
+ }
+
+ if (models.includes("Kwid")) {
+  optionsColors.push({ label: "White", value: "White" });
+ }
+
+ if (models.includes("Clio")) {
+  optionsColors.push({ label: "Silver", value: "Silver" });
+ }
+
+ if (models.includes("Sandero")) {
+  optionsColors.push({ label: "Black", value: "Black" });
+  optionsColors.push({ label: "Silver", value: "Silver" });
+ }
+ if (models.includes("Duster")) {
+  optionsColors.push({ label: "Deep Grey", value: "Deep Grey" });
  }
 
  return optionsColors;
